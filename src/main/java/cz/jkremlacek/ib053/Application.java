34d52b883a4afe338f982e7bc1b178298df19cee
Jakub Kremláček <jkremlacek@email.cz>
2018-03-12 20:04:30 +0100

    Project: Empty Gradle project added

---
+++ src/main/java/cz/jkremlacek/ib053/Application.java
@@ -0,0 +1,16 @@
+package cz.jkremlacek.ib053;
+
+/**
+ * @author Jakub Kremláček
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
